4dd45e16d89b371a15153a585503ca378f440c96
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2019-01-08 15:56:41 -0300

    alvaro: intial web api setup

---
+++ UltraCines/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UltraCines.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "not implemented" };
+        }
+    }
+}
